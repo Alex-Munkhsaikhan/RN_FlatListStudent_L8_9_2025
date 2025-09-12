db120eaa6b77ea6900e250700b668fef640daa77
Alex-Munkhsaikhan <erdembilegmunkhsaikhan@gmail.com>
2025-09-12 11:35:07 -0500

    Created a flatlist and itemSelected

--- FlatList_L8_L9/app/index.tsx
+++ FlatList_L8_L9/app/index.tsx
@@ -1,14 +1,39 @@
-import {
-  Text,
-  View,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
 import colors from "@/styles/colors";
 import defaultStyles from "@/styles/defaultStyles";
+import { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 
 export default function Index() {
+
+  // Declare the data type for each item in the array
+  type dataType = {
+    id: string, // unique identifier
+    title: string // Text displayed in flatlist
+  }
+
+  // Make the array
+  const DATA: dataType[] = [
+    {id: '1', title: 'First Item'},
+    {id: '2', title: 'Second Item'},
+    {id: '3', title: 'Third Item'},
+    {id: '4', title: 'Fourth Item'},
+  ];
+
+  // Create a state var to keep track of selected id
+  const[selectedId, setSelectedId] = useState<string>('1');
+
+  // Funciton to log what selected and display
+  const itemSelected = (item: dataType) => {
+    console.log(item.title);
+    setSelectedId(item.id);
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +41,21 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+   data = {DATA}
+            keyExtractor = {(item: dataType) => item.id}
+            renderItem = {({item}) => (
+              <TouchableOpacity onPress = {() => itemSelected(item)}>
+                <View style = {[styles.flatListRow, {
+                  backgroundColor: item.id === selectedId ? colors.primary: colors.secondary
+                }]}>
+                  <Text style = {[styles.titleText,{
+                    color: item.id === selectedId ? colors.secondary: colors.primary
+                  }]}>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -37,4 +76,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     padding: 10,
   },
+  flatListRow: {
+    backgroundClip: 'cornflowerblue',
+    margin: 5,
+    padding: 5,
+    width: 100,
+
+  }
 });
